51e180b47faa00afe00e10c77f26b29fdb78f5dd
ChenM <explayloko@Gmail.com>
2025-02-07 22:47:54 +0800

    添加手机号格式过滤

--- src/main/java/com/practice/practicemanage/pojo/User.java
+++ src/main/java/com/practice/practicemanage/pojo/User.java
@@ -1,6 +1,8 @@
 package com.practice.practicemanage.pojo;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,18 +14,24 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
+    @NotNull(message = "id不能为空")
     private Integer id;
 
     @Column(name = "username", length = 100)
+    @NotNull(message = "账号不能为空")
     private String userName;
 
     @Column(name = "password", length = 100)
+    @NotNull(message = "密码不能为空")
     private String passWord;
 
-    @Column(name = "phone", length = 20)
+    @Column(name = "phone", length = 11)
+    @NotNull(message = "手机号不能为空")
+    @Pattern(regexp = "^1((3[0-9])|(4[5-9])|(5[0-3,5-9])|(6[5,6])|(7[0-9])|(8[0-9])|(9[1,8,9]))\\d{8}$", message = "手机号格式不正确")
     private String phone;
 
     @Column(name = "status", nullable = false)
+    @NotNull(message = "状态不能为空")
     private Byte status;
 
 }
